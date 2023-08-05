2764d42758a208f245f1579cc226609cd69e4b83
VirajAdiga <virajadigamaclub@gmail.com>
2023-08-05 12:15:32 +0530

    Modify driver to handle scraping and summarising

--- driver.py
+++ driver.py
@@ -6,16 +6,22 @@ from factories.summarizer_factory import SummarizerFactory
 
 class Driver:
 
-    @staticmethod
-    def get_text_from_page(page_url, scraper_type):
+    def get_summary_of_page(self, page_url, scraper_type="goose", summarizer_type="extractive"):
+        logger.info(f"Received request for scraping and summarising content from page '{page_url}'")
+        extracted_main_article = self._get_text_from_page(page_url, scraper_type)
+        logger.info(f"This is the full extracted content from the requested web page (Total length of chars: {len(extracted_main_article)}): \n{extracted_main_article}")
+        summarised_content = self._summarize_text(extracted_main_article, summarizer_type)
+        logger.info(f"This is the summarised content (Total length of chars: {len(summarised_content)}): \n{summarised_content}")
+        return summarised_content
+
+    def _get_text_from_page(self, page_url, scraper_type):
         scraper = ScraperFactory.get_scraper(scraper_type)
         if scraper is None:
             raise ValueError("Please specify a valid scraper")
         logger.info(f"Initializing the scraper to extract data from page '{page_url}'")
         return scraper.scrape_get_text_from_page(page_url)
 
-    @staticmethod
-    def summarize_text(text, summarizer_type):
+    def _summarize_text(self, text, summarizer_type):
         summarizer = SummarizerFactory.get_summarizer(summarizer_type)
         if summarizer is None:
             raise ValueError("Please specify a valid summarizer")
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from loguru import logger
-
 from driver import Driver
 
 
-def main(page_url, scraper_type="goose", summarizer_type="extractive"):
-    logger.info(f"Received request for scraping and summarising content from page '{page_url}'")
-    extracted_main_article = Driver.get_text_from_page(page_url, scraper_type)
-    logger.info(f"This is the full extracted content from the requested web page (Total length of chars: {len(extracted_main_article)}): \n{extracted_main_article}")
-    summarised_content = Driver.summarize_text(extracted_main_article, summarizer_type)
-    logger.info(f"This is the summarised content (Total length of chars: {len(summarised_content)}): \n{summarised_content}")
+def main(page_url):
+    driver = Driver()
+    driver.get_summary_of_page(page_url)
 
 
 if __name__ == "__main__":
