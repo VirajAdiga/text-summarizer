744b39e1786b625aef39f63e4918a26545a50208
VirajAdiga <virajadigamaclub@gmail.com>
2023-09-05 11:16:26 +0530

    Add clean data logic in base media to text class

--- base/media_to_text.py
+++ base/media_to_text.py
@@ -1,4 +1,10 @@
+import re
+
 from abc import ABC, abstractmethod
+from loguru import logger
+
+
+UNWANTED_SPACES_PATTERN = r"\s{2,}"
 
 
 class MediaToText(ABC):
@@ -9,3 +15,7 @@ class MediaToText(ABC):
     @abstractmethod
     def get_media_text(self, media_url):
         raise NotImplementedError
+
+    def _clean_data(self, text):
+        logger.info(f"Cleaning extracted data")
+        return re.sub(UNWANTED_SPACES_PATTERN, " ", text).lstrip().rstrip()
