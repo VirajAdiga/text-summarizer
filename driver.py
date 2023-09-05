5d7cf68de6316fa7079d1e246f9e1762c21938ca
VirajAdiga <virajadigamaclub@gmail.com>
2023-09-05 11:17:19 +0530

    Add logic in driver class to find the type of url before extracting text

--- driver.py
+++ driver.py
@@ -1,36 +1,62 @@
 import textwrap
+import re
 
 from loguru import logger
 
+from factories.media_to_text_factory import MediaToTextConverterFactory
 from factories.scraper_factory import ScraperFactory
 from factories.summarizer_factory import SummarizerFactory
 
 
+YOUTUBE_URL_REGEX_PATTERN = r""
+
+
 class Driver:
 
-    def get_summary_of_page(self, page_url, scraper_type="goose", summarizer_type="transformers", max_length=None):
-        logger.info(f"Received request for scraping and summarising content from page '{page_url}'")
-        logger.info(f"Requested scraper type - {scraper_type}, summarizer type - {summarizer_type}")
+    def get_summary_of_page(self, url, max_length=None):
+        logger.info(f"Received request for summarising content from '{url}'")
 
-        extracted_main_article = self._get_text_from_page(page_url, scraper_type)
-        logger.info(f"This is the full extracted content from the requested web page (Total length of chars: {len(extracted_main_article)})")
-        logger.info(textwrap.fill(extracted_main_article, width=150))
+        regex_match = re.search(YOUTUBE_URL_REGEX_PATTERN, url)
+        if regex_match:
+            logger.info(f"Requested url is a youtube media")
+            extracted_text = self._get_media_text(url)
+        else:
+            logger.info(f"Requested url is a web page")
+            extracted_text = self._get_text_from_page(url)
+        logger.info(f"This is the full extracted content from the requested web page (Total length of chars: {len(extracted_text)})")
+        logger.info(textwrap.fill(extracted_text, width=150))
 
-        summarised_content = self._summarize_text(extracted_main_article, summarizer_type, max_length)
+        summarised_content = self._summarize_text(extracted_text, max_length)
         logger.info(f"This is the summarised content (Total length of chars: {len(summarised_content)})")
         logger.info(textwrap.fill(summarised_content, width=150))
         return summarised_content
 
-    def _get_text_from_page(self, page_url, scraper_type):
+    def _get_media_text(self, media_url):
+        media_to_text_converter_type = "youtube_media_transcript"
+
+        logger.info(f"Requested media to text converter type - {media_to_text_converter_type}")
+        media_to_text_converter = MediaToTextConverterFactory.get_converter(media_to_text_converter_type)
+        if media_to_text_converter is None:
+            raise ValueError("Please specify a valid media to text converter")
+        logger.info(f"Initializing the media to text converter to extract data from '{media_url}'")
+        return media_to_text_converter.get_media_text(media_url)
+
+    def _get_text_from_page(self, page_url):
+        scraper_type = "goose"
+
+        logger.info(f"Requested scraper type - {scraper_type}")
         scraper = ScraperFactory.get_scraper(scraper_type)
         if scraper is None:
             raise ValueError("Please specify a valid scraper")
         logger.info(f"Initializing the scraper to extract data from page '{page_url}'")
         return scraper.scrape_get_text_from_page(page_url)
 
-    def _summarize_text(self, text, summarizer_type, max_length):
+    def _summarize_text(self, text, max_length):
+        summarizer_type = "transformers"
+
+        logger.info(f"Requested summarizer type - {summarizer_type}")
         summarizer = SummarizerFactory.get_summarizer(summarizer_type)
         if summarizer is None:
             raise ValueError("Please specify a valid summarizer")
-        logger.info("Summarising text started")
+        logger.info("Initializing the summarizer to summarize given text")
         return summarizer.get_summarized_text(text, max_length)
